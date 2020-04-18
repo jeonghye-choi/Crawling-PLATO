b1f2abf1687cc85fc486931fbe435346afb5cfe4
jeonghye-choi <ppllhm.0@gmail.com>
2020-04-19 05:57:10 +0900

    Feat: Login
    아이디와 비밀번호를 입력하고 엔터를 눌러 로그인 하게함.
    
    아이디와 비번은 변경을 쉽게 하기위해 변수로 선언함.

--- all_weeks.py
+++ all_weeks.py
@@ -1,7 +1,11 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-# 홈페이지 열기
+# 홈페이지 열어서 로그인
+## user 정보
+usr = 'ID'
+pwd = 'PASSWORD'
 
 ## 홈페이지 정보
 path= 'https://plato.pusan.ac.kr/'
@@ -13,3 +17,11 @@ time.sleep(2)
 
 ## 올바른 페이지로 갔는지 확인
 assert "부산대학교" in driver.title
+
+## 로그인
+inputElement= driver.find_element_by_id('input-username')
+inputElement.send_keys(usr)
+inputElement= driver.find_element_by_id('input-password')
+inputElement.send_keys(pwd)
+inputElement.send_keys(Keys.RETURN)
+time.sleep(2)
