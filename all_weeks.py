3f23748e3e9af97d6ec1b650c46ca52c529fa7a7
jeonghye-choi <ppllhm.0@gmail.com>
2020-04-19 06:11:50 +0900

    Feat: Print vod, label (click unnecessary)
    페이지 이동 없이 현재 페이지에서 처리 가능한 vod와 label을 먼저 프린트 해줌
    
    li태그의 class이름으로 종류가 구분되어 있었다.
    그래서 'class_'를 사용해서 해당 값이 들어있는 li태그를 찾는 것으로 코드를 잤다.
    
    try except를 이용해서 에러때문에 갑자기 멈춰버리는 현상을 피한다.

--- all_weeks.py
+++ all_weeks.py
@@ -40,7 +40,7 @@ while lect_num>0:
     ### 이전페이지로
     lect_num -= 1
     driver.back()
-    
+
 
     #### 주차별 찾기!
     for weeks in range(1,15):
@@ -59,3 +59,19 @@ while lect_num>0:
         #### 종류구분 (종류에 따라서 처리가 다름)
         content= soup.find('li', id='section-'+ str(weeks))
         print("content : " + content.text)
+
+        # 1. 현재 페이지에서 처리 가능한 것들
+        #### 강의(클릭X, 해당페이지에 기한정보있음, 동영상url로 이동했을 때 출석체크되는지 확인해보기) - vod O
+        try:
+            vod= content.find_all('li', class_="vod")
+            for i in vod:
+                print("vod: ", i.text, "\n")
+        except:
+            pass
+        #### 참고 공지사항 - label O
+        try:
+            label= content.find_all('li', class_="label")
+            for i in label:
+                print("label: ", i.text, "\n")
+        except:
+            pass
