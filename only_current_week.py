8f9f1e09a867d8592ef29bbd6e14fe8d4254549e
jeonghye-choi <ppllhm.0@gmail.com>
2020-04-19 05:15:52 +0900

    feat: Access PLATO using webdriver
    크롬 웹드라이버를 이용해 chrome실행
    
    PLATO 홈페이지 열기
    
    올바른 홈페이지로 접속했는지 확인(using assert)

--- only_current_week.py
+++ only_current_week.py
@@ -1,2 +1,13 @@
-# 깃에 올라가는 계정이 DahuinWorld로 들어간다..
-# 수정시도!
+import time
+from selenium import webdriver
+
+
+## 부산대학교 홈페이지 열기
+
+path = "https://plato.pusan.ac.kr/"
+# Chrome WebDriver를 이용해 Chrome 실행
+driver = webdriver.Chrome('C:\Program Files (x86)\Python\chromedriver.exe')   
+driver.get(path)
+time.sleep(2)
+
+assert "부산대학교" in driver.title
