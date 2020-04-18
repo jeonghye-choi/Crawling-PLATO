9d2a094a373a8969ec96a919ff327e733ca29b36
jeonghye-choi <ppllhm.0@gmail.com>
2020-04-19 05:21:10 +0900

    feat: Login
    로그인 창에 아이디 비밀번호를 입력하고 엔터를 누르게 함
    
    유저ID 와 비번은 변경을 쉽게 하기 위해서 변수로 선언함

--- only_current_week.py
+++ only_current_week.py
@@ -1,5 +1,9 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+usr = 'ID'
+pwd = 'PASSWORD'
 
 
 ## 부산대학교 홈페이지 열기
@@ -11,3 +15,14 @@ driver.get(path)
 time.sleep(2)
 
 assert "부산대학교" in driver.title
+
+## 로그인하기
+
+# html element 이름을 찾습니다! 아이디비번
+inputElement = driver.find_element_by_id('input-username') # 아이디 입력란 id 찾아 커서 둠
+inputElement.send_keys(usr) # 커서 위치한 곳에 값을 넣음
+inputElement = driver.find_element_by_id('input-password') # 비번 입력란 id 찾아 커서 둠
+inputElement.send_keys(pwd) # 커서 위치한 곳에 값을 넣음
+inputElement.send_keys(Keys.RETURN) # Enter 키를 누름
+
+time.sleep(2)
